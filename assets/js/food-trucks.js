b4f5548b39825c4037b1f97d4e7359bc3de8010e
Patrick Joyce <pjaudiomv@gmail.com>
2023-10-08 12:01:24 -0400

    add css to hr

--- assets/js/food-trucks.js
+++ assets/js/food-trucks.js
@@ -2,22 +2,23 @@ const CONFIG = {
     SHEET_ID: "1eNSh7TkObRNm_jlP_-7CSs9nQMcPElrZFMrfeC9R-ow",
     SHEET_NAME: "events",
     API_KEY: "AIzaSyDolzT1yWJuPi6y3tP85fuh3JbPJfJnWeM",
-    URL() {
+    get URL() {
         return `https://sheets.googleapis.com/v4/spreadsheets/${this.SHEET_ID}/values/${this.SHEET_NAME}?key=${this.API_KEY}`;
     }
 };
 
 const convertSheetDataToListOfDicts = sheetData => {
     const headers = sheetData.values?.[0] || [];
-    return sheetData.values?.slice(1).map(row =>
-        headers.reduce((acc, header, index) => ({ ...acc, [header]: row[index] }), {})
-    ) || [];
+    const rows = sheetData.values?.slice(1) || [];
+    return rows.map(row =>
+        Object.fromEntries(row.map((cell, index) => [headers[index], cell]))
+    );
 };
 
 const isValidDate = dateStr => {
     const [month, day, year] = dateStr.split('/').map(Number);
     const date = new Date(year, month - 1, day);
-    return date && date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
+    return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
 };
 
 const parseDate = dateStr => {
@@ -25,21 +26,26 @@ const parseDate = dateStr => {
     return { day, month, year };
 };
 
-const eventToEpoch = event => {
-    const { day, month, year } = parseDate(event.date);
-    return new Date(year, month - 1, day + 1).getTime();
-};
+const eventToEpoch = event => new Date(parseDate(event.date)).getTime();
 
 const sortMonthYearKeys = (a, b) => {
-    const [monthYearA, yearA = 0] = a.split(' - ').map(Number);
-    const [monthYearB, yearB = 0] = b.split(' - ').map(Number);
-    return yearA - yearB || monthYearA - monthYearB;
+    const [monthA, yearA = 0] = a.split(' - ').map(Number);
+    const [monthB, yearB = 0] = b.split(' - ').map(Number);
+    return yearA - yearB || monthA - monthB;
 };
 
-const getMonthNameFromNumber = monthNumber => {
-    const date = new Date(new Date().getFullYear(), monthNumber - 1);
-    return date.toLocaleString('default', { month: 'long' });
-}
+const getMonthNameFromNumber = monthNumber =>
+    new Date(0, monthNumber - 1).toLocaleString('default', { month: 'long' });
+
+const createGroupedEvents = (validEvents, currentEpoch, currentYear) =>
+    validEvents.reduce((acc, event) => {
+        if (eventToEpoch(event) < currentEpoch) return acc;
+        const { month, year } = parseDate(event.date);
+        const key = `${month} - ${year}`;
+        acc[key] = acc[key] || [];
+        acc[key].push(event);
+        return acc;
+    }, {});
 
 const displayEventsByMonth = events => {
     const currentEpoch = Date.now();
@@ -54,19 +60,15 @@ const displayEventsByMonth = events => {
         return dateA.year - dateB.year || dateA.month - dateB.month || dateA.day - dateB.day;
     });
 
-    const groupedEvents = validEvents.reduce((acc, event) => {
-        if (eventToEpoch(event) < currentEpoch) return acc;
-        const key = `${parseDate(event.date).month} ${parseDate(event.date).year > currentYear ? `- ${parseDate(event.date).year}` : ''}`;
-        acc[key] = acc[key] || [];
-        acc[key].push(event);
-        return acc;
-    }, {});
-
+    const groupedEvents = createGroupedEvents(validEvents, currentEpoch, currentYear);
     const eventGroups = document.getElementById('eventGroups');
+
     Object.keys(groupedEvents).sort(sortMonthYearKeys).forEach(monthYearKey => {
+        let month = monthYearKey.split(' ')[0]
+        let year = monthYearKey.split(' ')[2]
         const monthDiv = document.createElement('div');
         const monthHeading = document.createElement('h2');
-        monthHeading.textContent = getMonthNameFromNumber(monthYearKey);
+        monthHeading.textContent = `${getMonthNameFromNumber(month)} ${year > currentYear ? `- ${year}` : ""}`;
         monthDiv.appendChild(monthHeading);
 
         const ul = document.createElement('ul');
@@ -74,24 +76,21 @@ const displayEventsByMonth = events => {
         let processedInfoDays = new Set();
 
         groupedEvents[monthYearKey].forEach(event => {
-            const { day } = parseDate(event.date);
-            let info = event.info?.trim() ? ` (${event.info})` : ''
+            const { day, month, year } = parseDate(event.date);
+            let info = event.info?.trim() ? ` (${event.info})` : '';
+            const eventLink = event.url?.trim() ? `<a href="${event.url}" target="_blank">${event.name}</a>` : event.name;
 
-            if (previousEventDay && (day - previousEventDay >= 1    )) {
-                const hr = document.createElement('hr');
-                const div = document.createElement('div');
+            if (previousEventDay && (day - previousEventDay >= 1)) {
+                const hr = document.createElement('hr')
                 hr.className = 'weeks-hr';
                 ul.appendChild(hr);
-                if (!processedInfoDays.has(day) && event.info && event.info.trim() !== "") {
-                    div.innerHTML = info;
+                if (!processedInfoDays.has(day) && info) {
+                    ul.appendChild(document.createElement('div')).innerHTML = info;
                     processedInfoDays.add(day);
                 }
-                ul.appendChild(div);
             }
 
-            const li = document.createElement('li');
-            li.innerHTML = event.url?.trim() ? `${day} - <a href="${event.url}" target="_blank">${event.name}</a>` : `${day} - ${event.name}`;
-            ul.appendChild(li);
+            ul.appendChild(document.createElement('li')).innerHTML = `${day} - ${eventLink}`;
             previousEventDay = day;
         });
 
@@ -100,7 +99,7 @@ const displayEventsByMonth = events => {
     });
 };
 
-fetch(CONFIG.URL())
+fetch(CONFIG.URL)
     .then(response => response.json())
     .then(data => {
         const events = convertSheetDataToListOfDicts(data);
