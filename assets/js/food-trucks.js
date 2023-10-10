52ca9608c6f69be4757c870889983aa7e26f4f24
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2023-10-10 11:06:12 -0400

    Pj/ci (#1)
    * add ci
    
    * updae action
    
    * update
    
    * pages deploy

--- assets/js/food-trucks.js
+++ assets/js/food-trucks.js
@@ -1,148 +1,146 @@
 const CONFIG = {
-    SHEET_ID: "1eNSh7TkObRNm_jlP_-7CSs9nQMcPElrZFMrfeC9R-ow",
-    SHEET_NAME: "events",
-    API_KEY: "AIzaSyDolzT1yWJuPi6y3tP85fuh3JbPJfJnWeM",
-    get URL() {
-        return `https://sheets.googleapis.com/v4/spreadsheets/${this.SHEET_ID}/values/${this.SHEET_NAME}?key=${this.API_KEY}`;
-    }
+  SHEET_ID: "1eNSh7TkObRNm_jlP_-7CSs9nQMcPElrZFMrfeC9R-ow",
+  SHEET_NAME: "events",
+  API_KEY: "AIzaSyDolzT1yWJuPi6y3tP85fuh3JbPJfJnWeM",
+  get URL() {
+    return `https://sheets.googleapis.com/v4/spreadsheets/${this.SHEET_ID}/values/${this.SHEET_NAME}?key=${this.API_KEY}`;
+  },
 };
 
-const convertSheetDataToListOfDicts = sheetData => {
-    const headers = sheetData.values?.[0] || [];
-    const rows = sheetData.values?.slice(1) || [];
-    return rows.map(row =>
-        Object.fromEntries(row.map((cell, index) => [headers[index], cell]))
+const convertSheetDataToListOfDicts = (sheetData) => {
+  const headers = sheetData.values?.[0] || [];
+  const rows = sheetData.values?.slice(1) || [];
+  return rows.map((row) =>
+    Object.fromEntries(row.map((cell, index) => [headers[index], cell]))
+  );
+};
+
+const parseDate = (dateStr) => {
+  const [month, day, year] = dateStr.split("/").map(Number);
+  return { day, month, year };
+};
+
+const eventToEpoch = (event) => new Date(parseDate(event.date)).getTime();
+
+const isValidDate = (dateStr) => {
+  const parsed = parseDate(dateStr);
+  const date = new Date(parsed.year, parsed.month - 1, parsed.day);
+  return (
+    date.getFullYear() === parsed.year &&
+    date.getMonth() === parsed.month - 1 &&
+    date.getDate() === parsed.day
+  );
+};
+
+const isValidURL = (str) => {
+  try {
+    new URL(str);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
+
+const getMonthNameFromNumber = (monthNumber) =>
+  new Date(0, monthNumber - 1).toLocaleString("default", { month: "long" });
+
+const ordinalSuffix = (num) => {
+  const j = num % 10,
+    k = num % 100;
+  if (j === 1 && k !== 11) return `${num}<sup>st</sup>`;
+  if (j === 2 && k !== 12) return `${num}<sup>nd</sup>`;
+  if (j === 3 && k !== 13) return `${num}<sup>rd</sup>`;
+  return `${num}<sup>th</sup>`;
+};
+
+const displayEventsByMonth = (events) => {
+  const currentEpoch = Date.now();
+  const currentYear = new Date().getFullYear();
+  const validEvents = events.filter(
+    (event) =>
+      event.date &&
+      event.name &&
+      isValidDate(event.date.trim()) &&
+      event.name.trim()
+  );
+
+  validEvents.sort((a, b) => {
+    const dateA = parseDate(a.date);
+    const dateB = parseDate(b.date);
+    return (
+      dateA.year - dateB.year ||
+      dateA.month - dateB.month ||
+      dateA.day - dateB.day
     );
-};
+  });
 
-const isValidDate = dateStr => {
-    const [month, day, year] = dateStr.split('/').map(Number);
-    const date = new Date(year, month - 1, day);
-    return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
-};
+  const groupedEvents = validEvents.reduce((acc, event) => {
+    if (eventToEpoch(event) < currentEpoch) return acc;
+    const { month, year } = parseDate(event.date);
+    const key = `${month} - ${year}`;
+    acc[key] = acc[key] || [];
+    acc[key].push(event);
+    return acc;
+  }, {});
 
-const parseDate = dateStr => {
-    const [month, day, year] = dateStr.split('/').map(Number);
-    return { day, month, year };
-};
+  const eventGroups = document.getElementById("eventGroups");
 
-const eventToEpoch = event => new Date(parseDate(event.date)).getTime();
+  Object.keys(groupedEvents).forEach((monthYearKey) => {
+    const [month, , year] = monthYearKey.split(" ");
+    const monthDiv = document.createElement("div");
+    const monthHeading = document.createElement("h2");
+    monthHeading.textContent = `${getMonthNameFromNumber(month)} ${
+      year > currentYear ? `- ${year}` : ""
+    }`;
+    monthDiv.appendChild(monthHeading);
 
-const sortMonthYearKeys = (a, b) => {
-    const [monthA, yearA = 0] = a.split(' - ').map(Number);
-    const [monthB, yearB = 0] = b.split(' - ').map(Number);
-    return yearA - yearB || monthA - monthB;
-};
+    const daysOl = document.createElement("ol");
+    daysOl.className = "circle-bullets";
+    let previousEventDay = null;
+    let processedInfoDays = new Set();
 
-const getMonthNameFromNumber = monthNumber =>
-    new Date(0, monthNumber - 1).toLocaleString('default', { month: 'long' });
+    groupedEvents[monthYearKey].forEach((event) => {
+      const { day } = parseDate(event.date);
+      const info = event.info?.trim() ? ` (${event.info})` : "";
+      const eventLink =
+        event.url?.trim() && isValidURL(event.url)
+          ? `<a href="${event.url}" target="_blank">${event.name}</a>`
+          : event.name;
 
-const createGroupedEvents = (validEvents, currentEpoch, currentYear) =>
-    validEvents.reduce((acc, event) => {
-        if (eventToEpoch(event) < currentEpoch) return acc;
-        const { month, year } = parseDate(event.date);
-        const key = `${month} - ${year}`;
-        acc[key] = acc[key] || [];
-        acc[key].push(event);
-        return acc;
-    }, {});
+      if (previousEventDay !== day) {
+        const dayLi = document.createElement("li");
+        dayLi.innerHTML =
+          !processedInfoDays.has(day) && info
+            ? `${ordinalSuffix(day)} - ${info}`
+            : ordinalSuffix(day);
+        processedInfoDays.add(day);
+        daysOl.appendChild(dayLi);
 
-const ordinalSuffix = num => {
-    const j = num % 10,
-        k = num % 100;
-    if (j === 1 && k !== 11) {
-        return num + "<sup>st</sup>";
-    }
-    if (j === 2 && k !== 12) {
-        return num + "<sup>nd</sup>";
-    }
-    if (j === 3 && k !== 13) {
-        return num + "<sup>rd</sup>";
-    }
-    return num + "<sup>th</sup>";
-}
+        const eventsOl = document.createElement("ol");
+        eventsOl.className = "circle-bullets-inner";
+        dayLi.appendChild(eventsOl);
+        const eventLi = document.createElement("li");
+        eventLi.innerHTML = eventLink;
+        eventsOl.appendChild(eventLi);
+      } else {
+        const eventsOl = daysOl.lastChild.lastChild;
+        const eventLi = document.createElement("li");
+        eventLi.innerHTML = eventLink;
+        eventsOl.appendChild(eventLi);
+      }
 
-const isValidURL = str => {
-    try {
-        new URL(str);
-        return true;
-    } catch (_) {
-        return false;
-    }
-}
-
-const displayEventsByMonth = events => {
-    const currentEpoch = Date.now();
-    const currentYear = new Date().getFullYear();
-    const validEvents = events.filter(event =>
-        event.date && event.name && isValidDate(event.date.trim()) && event.name.trim()
-    );
-
-    validEvents.sort((a, b) => {
-        const dateA = parseDate(a.date);
-        const dateB = parseDate(b.date);
-        return dateA.year - dateB.year || dateA.month - dateB.month || dateA.day - dateB.day;
+      previousEventDay = day;
     });
 
-    const groupedEvents = createGroupedEvents(validEvents, currentEpoch, currentYear);
-    const eventGroups = document.getElementById('eventGroups');
-
-    Object.keys(groupedEvents).sort(sortMonthYearKeys).forEach(monthYearKey => {
-        const month = monthYearKey.split(' ')[0];
-        const year = monthYearKey.split(' ')[2];
-        const monthDiv = document.createElement('div');
-        const monthHeading = document.createElement('h2');
-        monthHeading.textContent = `${getMonthNameFromNumber(month)} ${year > currentYear ? `- ${year}` : ""}`;
-        monthDiv.appendChild(monthHeading);
-
-        const ul = document.createElement('ul');
-        const daysOl = document.createElement('ol');
-        daysOl.className = 'circle-bullets';
-        let previousEventDay = null;
-        let processedInfoDays = new Set();
-
-        groupedEvents[monthYearKey].forEach(event => {
-            const { day } = parseDate(event.date);
-            let info = event.info?.trim() ? ` (${event.info})` : '';
-            const eventLink = event.url?.trim() && isValidURL(event.url) ? `<a href="${event.url}" target="_blank">${event.name}</a>` : event.name;
-
-            if (previousEventDay !== day) {
-                const dayLi = document.createElement('li');
-                if (!processedInfoDays.has(day) && info) {
-                    dayLi.innerHTML = `${ordinalSuffix(day)} - ${info}`;
-                    processedInfoDays.add(day);
-                } else {
-                    dayLi.innerHTML = ordinalSuffix(day);
-                }
-                daysOl.appendChild(dayLi);
-
-                const eventsOl = document.createElement('ol');
-                eventsOl.className = 'circle-bullets-inner';
-                dayLi.appendChild(eventsOl);
-
-                const eventLi = document.createElement('li');
-                eventLi.innerHTML = eventLink;
-                eventsOl.appendChild(eventLi);
-            } else {
-                const eventsOl = daysOl.lastChild.lastChild;
-                const eventLi = document.createElement('li');
-                eventLi.innerHTML = eventLink;
-                eventsOl.appendChild(eventLi);
-            }
-
-            previousEventDay = day;
-        });
-
-        monthDiv.appendChild(daysOl);
-        eventGroups.appendChild(monthDiv);
-    });
+    monthDiv.appendChild(daysOl);
+    eventGroups.appendChild(monthDiv);
+  });
 };
-
 
 fetch(CONFIG.URL)
-    .then(response => response.json())
-    .then(data => {
-        const events = convertSheetDataToListOfDicts(data);
-        displayEventsByMonth(events);
-    })
-    .catch(error => console.error('Error fetching data:', error));
+  .then((response) => response.json())
+  .then((data) => {
+    const events = convertSheetDataToListOfDicts(data);
+    displayEventsByMonth(events);
+  })
+  .catch((error) => console.error("Error fetching data:", error));
--- assets/js/imap.js
+++ assets/js/imap.js
@@ -2,88 +2,93 @@
  * Created by jcolfer on 12/19/16.
  */
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiZGlkZXZjbyIsImEiOiJjaXM3cWY3NDEwNDc0Mnpwa2w5YnllMXZkIn0.4pWeAL6-vhtobhpFd2HDuA';
+L.mapbox.accessToken =
+  "pk.eyJ1IjoiZGlkZXZjbyIsImEiOiJjaXM3cWY3NDEwNDc0Mnpwa2w5YnllMXZkIn0.4pWeAL6-vhtobhpFd2HDuA";
 
-map = L.mapbox.map('imap', 'mapbox.streets', {
-    'maxZoom': 19,
-    'minZoom': 14,
-    'zoom': 15,
-    'scrollWheelZoom' : 'center'
-}).setView([33.0565, -80.103917]);
-
+map = L.mapbox
+  .map("imap", "mapbox.streets", {
+    maxZoom: 19,
+    minZoom: 14,
+    zoom: 15,
+    scrollWheelZoom: "center",
+  })
+  .setView([33.0565, -80.103917]);
 
 var layer = L.mapbox.featureLayer().addTo(map);
-map.on('click', function (e) {
-    console.log(e.latlng);
+map.on("click", function (e) {
+  console.log(e.latlng);
 });
-var stamenLayer = L.tileLayer('https://www.carnescrossroads.com/wp-content/themes/carnes-crossroads/assets/images/map/2021-02-25/{z}/{x}/{y}.png',{
+var stamenLayer = L.tileLayer(
+  "https://www.carnescrossroads.com/wp-content/themes/carnes-crossroads/assets/images/map/2021-02-25/{z}/{x}/{y}.png",
+  {
     minZoom: 14,
-    maxZoom: 19
-}).addTo(map);
+    maxZoom: 19,
+  }
+).addTo(map);
 
 var geoJson = {
-    type: 'FeatureCollection',
-    features: []
+  type: "FeatureCollection",
+  features: [],
 };
 
-if(typeof locations != 'undefined') {
-    for (var i = 0; i < locations.length; i++) {
-        geoJson.features.push({
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [parseFloat(locations[i][3]), parseFloat(locations[i][2])]
-            },
-            "properties": {
-                "listing-type": locations[i][4],
-                "marker-color": setMarkerColor(locations[i][4]),
-                // 'marker-symbol': 'building',
-                "pop-up": locations[i][5]
-            }
-        });
-    }
+if (typeof locations != "undefined") {
+  for (var i = 0; i < locations.length; i++) {
+    geoJson.features.push({
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [parseFloat(locations[i][3]), parseFloat(locations[i][2])],
+      },
+      properties: {
+        "listing-type": locations[i][4],
+        "marker-color": setMarkerColor(locations[i][4]),
+        // 'marker-symbol': 'building',
+        "pop-up": locations[i][5],
+      },
+    });
+  }
 }
 
 layer.setGeoJSON(geoJson);
 
-layer.on('click', function(e) {
-    if (!e.layer) return;
+layer.on("click", function (e) {
+  if (!e.layer) return;
 
-    var popup = L.popup()
-        .setLatLng(e.latlng)
-        .setContent(e.layer.feature.properties["pop-up"])
-        .openOn(map)
+  var popup = L.popup()
+    .setLatLng(e.latlng)
+    .setContent(e.layer.feature.properties["pop-up"])
+    .openOn(map);
 });
 
 map.setZoom(17);
 
 function setMarkerColor(listingType) {
-    var color = null;
+  var color = null;
 
-    switch (listingType) {
-        case 'community_amenity':
-            color = '#56c1b1';
-            break;
+  switch (listingType) {
+    case "community_amenity":
+      color = "#56c1b1";
+      break;
 
-        case 'neighborhood':
-            color = '#536377';
-            break;
+    case "neighborhood":
+      color = "#536377";
+      break;
 
-        case 'park_lake':
-            color = '#695e49';
-            break;
+    case "park_lake":
+      color = "#695e49";
+      break;
 
-        case 'town_medical_office':
-            color = '#bf7616';
-            break;
+    case "town_medical_office":
+      color = "#bf7616";
+      break;
 
-        case 'real_estate':
-            color = '#FF0000';
-            break;
+    case "real_estate":
+      color = "#FF0000";
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 
-    return color;
+  return color;
 }
